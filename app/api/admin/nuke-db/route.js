149ac754e3b005628facb4f6a2d97ba98e4f7124
Scott Holbrook <securetoolcompany@gmail.com>
2026-08-04 22:34:40 -0700

    more dead admin api

--- app/api/admin/nuke-db/route.js
+++ app/api/admin/nuke-db/route.js
@@ -1,21 +1,20 @@
-import { NextResponse } from 'next/server';
-import { connectToDatabase } from '@/lib/mongodb';
+//import { NextResponse } from 'next/server';
+//import { connectToDatabase } from '@/lib/mongodb';
 
-export async function GET() {
-    try {
-        const { db } = await connectToDatabase();
-
-        // --- THE BIG RED BUTTON ---
+//export async function GET() {
+  //  try {
+    //    const { db } = await connectToDatabase();
+      // --- THE BIG RED BUTTON ---
         // Deletes EVERYTHING in the 'ip_assets' collection
-        const result = await db.collection('ip_assets').deleteMany({});
+//        const result = await db.collection('ip_assets').deleteMany({});
 
-        return NextResponse.json({ 
-            success: true, 
-            message: "☢️ DATABASE NUKED. All IP Assets have been deleted.", 
+  //      return NextResponse.json({ 
+    //        success: true, 
+      //      message: "☢️ DATABASE NUKED. All IP Assets have been deleted.", 
             deleted_count: result.deletedCount 
-        });
+        //});
 
-    } catch (e) {
-        return NextResponse.json({ error: e.message }, { status: 500 });
-    }
-}
+    //} catch (e) {
+    //    return NextResponse.json({ error: e.message }, { status: 500 });
+    //}
+//}
